fe0c5051d85913a9096ade67a8e1c290e1743b8a
mengqiu PENG <mengqiu.peng@thoughtworks.com>
2020-09-24 22:48:02 +0800

    [mengqiu]  add feedback

--- src/async.mock/__test__/user.test.js
+++ src/async.mock/__test__/user.test.js
@@ -1,29 +1,39 @@
-import { register, validate} from "../user";
 import axios from "axios";
+import { register } from "../user";
+import { verifyUsername, verifyPassword } from "../verify";
 
 jest.mock("../verify");
 jest.mock("axios");
-const mockValidate = jest.fn()
-let username = "fake username";
-let password = "fake password";
+// TODO feedback: 未使用的变量就删掉
+
+// const mockValidate = jest.fn()
+const username = "fake username";
+const password = "fake password";
 
 describe("register", () => {
   test("should post user when validated", async () => {
     // TODO 19: add test here
     axios.post.mockResolvedValueOnce({
       data: {
-        name: "sun"
-      }
-    })
-    let result = register(username, password);
-    await expect(result).resolves.toEqual({name: "sun"});
+        name: "sun",
+      },
+    });
+    const result = register(username, password);
+    await expect(result).resolves.toEqual({ name: "sun" });
   });
 
   test("should reject with Error when username is invalid", async () => {
     // TODO 20: add test here
-    validate(username,password).mockReturnValue(false);
-    await register(username, password).catch(err => {
-       expect(err).toThrow("wrong username or password");
-    });
+
+    // TODO feedback:我们mock的时候，需要mock所依赖的module的function, 而不是自己文件里面的function
+
+    // validate(username,password).mockReturnValue(false);
+    verifyUsername.mockImplementation(() => false);
+    verifyPassword.mockImplementation(() => true);
+
+    // await register(username, password).catch(err => {
+    //    expect(err).toThrow("wrong username or password");
+    // });
+    await expect(register()).rejects.toThrow(Error);
   });
 });
--- src/async/__test__/users.test.js
+++ src/async/__test__/users.test.js
@@ -1,9 +1,16 @@
-const { default: getUsers } = require("../users");
+import axios from "axios";
+// TODO feedback: 使用import就好了，不需要使用require
+
+// const { default: getUsers } = require("../users");
+import getUsers from "../users";
+
 jest.mock("axios");
 
 describe("users", () => {
   test("should get users data with mock axios get", async () => {
     // TODO 13: add async test with manual mock
+    // TODO feedback: getUsers()依赖了axios的get方法，所以你需要mock它
+    axios.get.mockResolvedValue({ data: {} });
     const result = getUsers();
     await expect(result).resolves.toEqual({});
   });

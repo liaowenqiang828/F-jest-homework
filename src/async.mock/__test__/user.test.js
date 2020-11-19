c2764e69ee9585b9aa78c83b069f882a098ddc0a
liaowenqiang <wenqiang.liao@liaowenqiangdeMacBook-Pro.local>
2020-11-19 21:42:55 +0800

    [Wenqiang Liao] fix: complelish the tset

--- src/async.mock/__test__/user.test.js
+++ src/async.mock/__test__/user.test.js
@@ -1,29 +1,31 @@
-import { register, validate} from "../user";
 import axios from "axios";
+import { register } from "../user";
+import { verifyPassword } from "../verify";
 
 jest.mock("../verify");
 jest.mock("axios");
-const mockValidate = jest.fn()
-let username = "fake username";
-let password = "fake password";
+const username = "fake username";
+const password = "fake password";
 
 describe("register", () => {
   test("should post user when validated", async () => {
     // TODO 19: add test here
     axios.post.mockResolvedValueOnce({
       data: {
-        name: "sun"
-      }
-    })
-    let result = register(username, password);
-    await expect(result).resolves.toEqual({name: "sun"});
+        name: "sun",
+      },
+    });
+    const result = register(username, password);
+    await expect(result).resolves.toEqual({ name: "sun" });
   });
 
   test("should reject with Error when username is invalid", async () => {
     // TODO 20: add test here
-    validate(username,password).mockReturnValue(false);
-    await register(username, password).catch(err => {
-       expect(err).toThrow("wrong username or password");
-    });
+    const mockUsername = "fake username";
+    const mockPassword = "fake password";
+    verifyPassword.mockImplementation(() => false);
+    await expect(register(mockUsername, mockPassword)).rejects.toThrow(
+      "wrong username or password"
+    );
   });
 });
--- src/async/__test__/users.test.js
+++ src/async/__test__/users.test.js
@@ -1,9 +1,12 @@
+const { default: axios } = require("axios");
 const { default: getUsers } = require("../users");
+
 jest.mock("axios");
 
 describe("users", () => {
   test("should get users data with mock axios get", async () => {
     // TODO 13: add async test with manual mock
+    axios.get.mockResolvedValueOnce({ data: {} });
     const result = getUsers();
     await expect(result).resolves.toEqual({});
   });
